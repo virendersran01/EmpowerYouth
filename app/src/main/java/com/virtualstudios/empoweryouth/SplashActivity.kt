faf929f5a20249f7d38bf213fb1c1260112c856b
Virender Sran <virendersran01@gmail.com>
2021-03-06 16:32:45 +0530

    working on theme and styles.

--- app/src/main/java/com/virtualstudios/empoweryouth/SplashActivity.kt
+++ app/src/main/java/com/virtualstudios/empoweryouth/SplashActivity.kt
@@ -1,12 +1,18 @@
 package com.virtualstudios.empoweryouth
 
+import android.content.Intent
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.view.WindowInsets
 import android.view.WindowManager
 
 class SplashActivity : AppCompatActivity() {
+
+    private var mDelayHandler: Handler? = null
+    private val SPLASH_DELAY: Long = 3000 //3 seconds
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
@@ -14,6 +20,17 @@ class SplashActivity : AppCompatActivity() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
             window.insetsController?.hide(WindowInsets.Type.navigationBars())
         }
+
+        mDelayHandler = Handler()
+
+        //Navigate with delay
+        mDelayHandler!!.postDelayed(mRunnable, SPLASH_DELAY)
     }
 
+    internal val mRunnable: Runnable = Runnable {
+
+        val intent = Intent(applicationContext, MainActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
 }
