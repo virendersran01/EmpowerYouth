da0be76fc32d146f10da13078986caaeed510462
Virender Sran <virendersran01@gmail.com>
2021-03-06 16:18:07 +0530

    Created Splash Screen.

--- app/src/main/java/com/virtualstudios/empoweryouth/SplashActivity.kt
+++ app/src/main/java/com/virtualstudios/empoweryouth/SplashActivity.kt
@@ -1,11 +1,19 @@
 package com.virtualstudios.empoweryouth
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowInsets
+import android.view.WindowManager
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+        @Suppress("DEPRECATION")
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.hide(WindowInsets.Type.navigationBars())
+        }
     }
+
 }
