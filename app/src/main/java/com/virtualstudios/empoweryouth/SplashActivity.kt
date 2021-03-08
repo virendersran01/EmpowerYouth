9158b2cc31af67138ae7c3d89de2c904a042ef86
Virender Sran <virendersran01@gmail.com>
2021-03-08 22:37:15 +0530

    change splash activity code.

--- app/src/main/java/com/virtualstudios/empoweryouth/SplashActivity.kt
+++ app/src/main/java/com/virtualstudios/empoweryouth/SplashActivity.kt
@@ -5,32 +5,27 @@ import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.os.Looper
 import android.view.WindowInsets
 import android.view.WindowManager
 
 class SplashActivity : AppCompatActivity() {
 
-    private var mDelayHandler: Handler? = null
-    private val SPLASH_DELAY: Long = 3000 //3 seconds
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
-        @Suppress("DEPRECATION")
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
             window.insetsController?.hide(WindowInsets.Type.navigationBars())
         }
-
-        mDelayHandler = Handler()
-
-        //Navigate with delay
-        mDelayHandler!!.postDelayed(mRunnable, SPLASH_DELAY)
+        loadSplashScreen()
     }
 
-    internal val mRunnable: Runnable = Runnable {
-
-        val intent = Intent(applicationContext, MainActivity::class.java)
-        startActivity(intent)
-        finish()
+    private fun loadSplashScreen(){
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(applicationContext, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 8000)
     }
+
 }
